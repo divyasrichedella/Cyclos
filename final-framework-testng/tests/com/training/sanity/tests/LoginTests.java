e0cf27063117ca7aa30e44212aff62dc5f395a0d
Pavani09 <pavanisharma079@gmail.com>
2020-02-13 14:42:26 +0530

    Simple and medium test cases.

--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -5,25 +5,22 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.training.generics.ScreenShot;
-import com.training.pom.LoginPOM;
+import com.training.pom.LoginPOM1;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
 
-public class LoginTests {
+public class LoginTests{
 
 	private WebDriver driver;
 	private String baseUrl;
-	private LoginPOM loginPOM;
+	private LoginPOM1 loginPOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
 
@@ -37,7 +34,7 @@ public class LoginTests {
 	@BeforeMethod
 	public void setUp() throws Exception {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
-		loginPOM = new LoginPOM(driver); 
+		loginPOM = new LoginPOM1(driver); 
 		baseUrl = properties.getProperty("baseURL");
 		screenShot = new ScreenShot(driver); 
 		// open the browser 
@@ -50,39 +47,20 @@ public class LoginTests {
 		driver.quit();
 	}
 	@Test
-	public void validLoginTest() throws InterruptedException{
+	public void validLoginTest() {
 		loginPOM.sendUserName("admin");
-		loginPOM.But1();
-		loginPOM.But2();
-		loginPOM.But3();
-		loginPOM.But4();
-		loginPOM.submit();
-		loginPOM.member1();
-        Thread.sleep(3000);
-       loginPOM.grant1();  
-       loginPOM.loan1();
-       loginPOM.dep();
-       loginPOM.subm();
-       loginPOM.subm1();
-       Alert ae=driver.switchTo().alert();
-	   ae.accept();
-       loginPOM.viewloan1();
-       loginPOM.logout1();
-       Alert a=driver.switchTo().alert();
-       a.accept();
-		loginPOM.sendUserName("pavan");
-		loginPOM.But11();
-		loginPOM.But21();
-		loginPOM.But31();
-		loginPOM.But41();
-        loginPOM.submit1();
-		
-		loginPOM.accountLink();
-		loginPOM.loansLink();
-		loginPOM.viewIcon();
-		loginPOM.amountText();
-		loginPOM.repayBtn();
-		
-		screenShot.captureScreenShot("First");
+		loginPOM.Password1();
+		loginPOM.Password2();
+		loginPOM.Password3();
+		loginPOM.Password4();
+		loginPOM.clickLoginBtn(); 
+		loginPOM.personalBtn();
+		loginPOM.changepasswordbtn();
+		loginPOM.sendoldPasswordtxt("1234");
+		loginPOM.sendnewPasswordtxt("12345");
+		loginPOM.sendnewPasswordConfirmation("654321");
+		loginPOM.Submitbtn(); 
+		Alert a=driver.switchTo().alert();
+		a.accept();
 	}
 }
